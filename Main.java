8ecf9dd61f50bac972669e93fdeefc9119ea8125
PietroGolfeto <pggolfeto@gmail.com>
2023-06-22 17:36:45 -0300

    ideia para menu

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ class Main {
     // Sistema de gerenciamento hospitalar
     public static void main(String[] args) {
         // TO DO - Modificar LISTA_PACIENTES para LISTA_CONSULTAS no Medico.CSV
+        // JFORMDESIGNER faz desenhando o menu (NETBEANS ou ECLIPSE)
+        // JAVAFX no VSCODE mais prático que Swing
+        // Não tem tempo pra fazer na mão pelo Swing. Cada botão leva mais de hora pra fazer a lógica e qualquer mudança quebra
+        // Precisamos achar alguma ferramenta melhor pra fazer o menu
         MenuPrincipal menu = new MenuPrincipal();
     }
 }
