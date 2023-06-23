2af96253f68665a090f0a5ada0deed8af9a906b4
LucasGabriel-183967 <l183967@dac.unicamp.br>
2023-06-23 01:33:03 -0300

    Comecando a criar o menu de cadastro

--- MenuCadastrar.java
+++ MenuCadastrar.java
@@ -1,11 +1,14 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JScrollPane;
 import javax.swing.JMenuItem;
+
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -18,7 +21,19 @@ public class MenuCadastrar extends JFrame {
 
         JPanel painel = new JPanel();
 
-        JTextField campoNome = new JTextField(10);
+        painel.setLayout(new GridBagLayout());
+
+        JLabel labelNome = new JLabel();
+
+        labelNome.setText("Digite o nome: ");
+        JTextArea campoNome = new JTextArea(8, 25);
+
+        painel.add(labelNome);
         painel.add(painel, campoNome, ABORT);
+
+        add(painel);
+        setSize(1920, 1080);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 }
